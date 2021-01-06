f790d0150941a1a171542a1431daac1010e5e237
Ruslan Kurdyumov <ruslan88@gmail.com>
2021-01-06 08:16:04 +0000

    Clean up documentation.

--- application.py
+++ application.py
@@ -10,7 +10,10 @@ from helpers import apology, login_required, lookup, usd
 
 # Configure application
 app = Flask(__name__)
-app.secret_key = os.environ.get("SECRET_KEY") or b'_5#y2L"F4Q8z\n\xec]/'
+# Client-side sessions require a secret key
+if not os.environ.get("SECRET_KEY"):
+    raise RuntimeError("SECRET_KEY not set")
+app.secret_key = os.environ.get("SECRET_KEY")
 
 # Ensure templates are auto-reloaded
 app.config["TEMPLATES_AUTO_RELOAD"] = True
@@ -26,12 +29,12 @@ def after_request(response):
 # Custom filter
 app.jinja_env.filters["usd"] = usd
 
-# Configure CS50 Library to use Heroku Postgres database
+# Connect to the Heroku Postgres database
 if not os.environ.get("DATABASE_URL"):
     raise RuntimeError("DATABASE_URL not set")
 db = create_engine(os.environ.get("DATABASE_URL"))
 
-# Make sure API key is set
+# Make sure IEX API key is set
 if not os.environ.get("API_KEY"):
     raise RuntimeError("API_KEY not set")
 
@@ -56,6 +59,10 @@ def index():
         id=session["user_id"])
     stocks = []
     for row in rows:
+        # Cast as a workaround for Postgres (stores sum(bigint) => numeric):
+        # https://www.postgresql.org/docs/current/functions-aggregate.html#FUNCTIONS-AGGREGATE-TABLE
+        # And, of course, SQL Alchemy returns numeric types as decimal.Decimal:
+        # https://docs.sqlalchemy.org/en/13/core/type_basics.html#sqlalchemy.types.Numeric
         shares = int(row["shares"])
         if shares == 0:
             continue
@@ -87,9 +94,9 @@ def account():
             return apology("missing password")
 
         # Query database for new username
-        rows = db.execute(text("SELECT * FROM users WHERE username = :u"),
-                          u=request.form.get("username")).fetchall()
-        if len(rows) != 0:
+        row = db.execute(text("SELECT * FROM users WHERE username = :u"),
+                          u=request.form.get("username")).fetchone()
+        if row:
             return apology("username already exists")
 
         hash = db.execute(text("SELECT * FROM users WHERE id = :id"),
@@ -143,6 +150,7 @@ def buy():
     cash = db.execute(text("SELECT * FROM users WHERE id = :id"),
                       id=session["user_id"]).fetchone()["cash"]
     purchase_price = int(request.form.get("shares")) * quote["price"]
+    # Cast decimal.Decimal (from Postgres numeric) to float.
     if purchase_price > float(cash):
         return apology("can't afford", 400)
 
@@ -254,9 +262,9 @@ def register():
     if not request.form.get("username"):
         return apology("must provide username", 403)
     # Ensure username is not already taken
-    rows = db.execute(text("SELECT * FROM users WHERE username = :username"),
-                      username=request.form.get("username")).fetchall()
-    if len(rows) == 1:
+    row = db.execute(text("SELECT * FROM users WHERE username = :username"),
+                      username=request.form.get("username")).fetchone()
+    if row:
         return apology("username already exists", 403)
     # Ensure password was submitted
     elif not request.form.get("password"):
@@ -266,9 +274,10 @@ def register():
         return apology("password confirmation must match", 403)
 
     # Add user and automatically log in.
-    result = db.execute(text("INSERT INTO users (username, hash) VALUES (:u, :h)"),
-                        u=request.form.get("username"),
-                        h=generate_password_hash(request.form.get("password")))
+    result = db.execute(text(
+        "INSERT INTO users (username, hash) VALUES (:u, :h)"),
+        u=request.form.get("username"),
+        h=generate_password_hash(request.form.get("password")))
     # TODO: Use result.inserted_primary_key after converting to SQLAlchemy ORM.
     if db.url.get_backend_name() in ["postgres", "postgresql"]:
         id = db.execute("SELECT LASTVAL()").first()[0]
@@ -296,16 +305,14 @@ def sell():
         return apology("missing symbol", 400)
     elif not request.form.get("shares"):
         return apology("missing shares", 400)
-    elif int(request.form.get("shares")) < 1:
-        return apology("must sell at least one share", 400)
 
-    rows = db.execute(text(
+    owned_shares = db.execute(text(
         "SELECT sum(shares) as shares FROM transactions "
         "WHERE user_id=:id AND symbol=:symbol"),
         id=session["user_id"],
-        symbol=request.form.get("symbol"))
+        symbol=request.form.get("symbol")).fetchone()["shares"]
     requested_shares = int(request.form.get("shares"))
-    if requested_shares > rows.fetchone()["shares"]:
+    if requested_shares > owned_shares:
         return apology("too many shares", 400)
 
     quote = lookup(request.form.get("symbol"))
